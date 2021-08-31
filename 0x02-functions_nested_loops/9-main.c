c0cb432666da35b16f5e39d003bb315f394c1347
Abdisa Gemechu <myabdisa@gmail.com>
2021-08-31 09:46:19 -0700

    added all the files from 2 -12 to git

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+
+ * main - check the code for ALX School students.
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+  
+{
+  
+  times_table();
+  
+  return (0);
+  
+}
