09e083b558d633799b0cf23eff6b9d6320fa1985
Abdisa Gemechu <myabdisa@gmail.com>
2021-09-23 21:37:30 -0700

    fixed task2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_nconcat - ...
- * @s1: ...
- * @s2: ...
- * @n: ...
- *
- * Return: ...
+ * string_nconcat - concatenate two strings up to n bytes.
+ * @s1: source string
+ * @s2: string to truncate up to n bytes
+ * @n: number of bytes to truncate by
+ * Return: pointer to new buffer
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
-{ unsigned int i = 0, j = 0, k = 0, l = 0;
-char *str;
+{ char *p;
+unsigned int s1count, s2count, sizeBuffer, i;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-while (s1[i])
-i++;
-while (s2[k])
-k++;
-if (n >= k)
-l = i + k;
-else
-l = i + n;
-str = malloc(sizeof(char) * l + 1);
-if (str == NULL)
+for (s1count = 0; s1[s1count]; s1count++)
+;
+for (s2count = 0; s2[s2count]; s2count++)
+;
+s2count > n ? (s2count = n) : (n = s2count);
+sizeBuffer = s1count + s2count + 1;
+p = malloc(sizeBuffer *sizeof(char));
+if (p == NULL)
 return (NULL);
-k = 0;
-while (j < l)
-{
-if (j <= i)
-str[j] = s1[j];
-if (j >= i)
-{ str[j] = s2[k];
-k++; }
-j++; }
-str[j] = '\0';
-return (str); }
+for (i = 0; i < sizeBuffer - 1; i++)
+i < s1count ? (p[i] = s1[i]) : (p[i] = s2[i - s1count]);
+p[sizeBuffer] = '\0';
+return (p); }
