9ac7b857d0ebac95a36815c82abb6d1b32a0b1e5
Abdisa Gemechu <myabdisa@gmail.com>
2021-10-12 03:18:39 -0700

    finished the advanced tasks

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,24 @@
-#ifndef _S_LINK_LISTS_
+#ifndef LISTS_H
 
-#define _S_LINK_LISTS_
+#define LISTS_H
+
+
+
+/*
+
+ * File: lists.h
+
+ * Auth: Brennan D Baraban
+
+ * Desc: Header file containing prototypes and definitions for all functions
+
+ *       and types written in the 0x12-more_singly_linked_lists directory.
+
+ */
+
+
+
+#include <stdlib.h>
 
 
 
@@ -49,7 +67,19 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 
 int sum_listint(listint_t *head);
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+
 listint_t *reverse_listint(listint_t **head);
-#endif
+
+size_t print_listint_safe(const listint_t *head);
+
+size_t free_listint_safe(listint_t **h);
+
+listint_t *find_listint_loop(listint_t *head);
+
+
+
+#endif /* LISTS_H */
