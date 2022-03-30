b899d07d08f8e241401f689a63a0bfad9b50054b
abdisag1 <myabdisa@gmail.com>
2022-03-30 19:32:43 +0000

    modified task 1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,13 +14,14 @@ int linear_search(int *array, size_t size, int value)
 {
 
 size_t i = 0;
-if (array == NULL)
-return (-1);
+if (array != NULL)
+{
 for (i = 0 ; i <= size; i++)
 {
 printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 if (array[i] == value)
 return (i);
+}
 }
 return (-1);
 
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,5 +1,5 @@
-#ifndef _SEARCHALGO
-#define _SEARCHALGO
+#ifndef _SEARCHALGO_H_
+#define _SEARCHALGO_H_
 #include <stdio.h>
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
